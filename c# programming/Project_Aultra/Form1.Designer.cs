90609ca7ae209e7cfa52e87ceceb082c43b10bfb
taiabur rahman <taiabur-tju@qq.com>
2018-11-04 20:42:10 +0800

    c# programming

---
+++ c# programming/Project_Aultra/Form1.Designer.cs
@@ -0,0 +1,251 @@
+﻿namespace Project_Aultra
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.prgsCM = new System.Windows.Forms.ProgressBar();
+            this.lblmessage = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.txtobj = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.groupBox2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.prgsCM);
+            this.groupBox2.Controls.Add(this.lblmessage);
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Controls.Add(this.button1);
+            this.groupBox2.Controls.Add(this.groupBox1);
+            this.groupBox2.Location = new System.Drawing.Point(3, 213);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(353, 282);
+            this.groupBox2.TabIndex = 9;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Distance";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(113, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(142, 75);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "000";
+            this.label1.TextChanged += new System.EventHandler(this.label1_TextChanged);
+            // 
+            // prgsCM
+            // 
+            this.prgsCM.BackColor = System.Drawing.SystemColors.ControlText;
+            this.prgsCM.ForeColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.prgsCM.Location = new System.Drawing.Point(6, 94);
+            this.prgsCM.Name = "prgsCM";
+            this.prgsCM.Size = new System.Drawing.Size(339, 22);
+            this.prgsCM.TabIndex = 3;
+            // 
+            // lblmessage
+            // 
+            this.lblmessage.AutoSize = true;
+            this.lblmessage.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblmessage.Location = new System.Drawing.Point(11, 230);
+            this.lblmessage.Name = "lblmessage";
+            this.lblmessage.Size = new System.Drawing.Size(0, 18);
+            this.lblmessage.TabIndex = 13;
+            // 
+            // button2
+            // 
+            this.button2.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.button2.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.button2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Location = new System.Drawing.Point(185, 181);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(162, 31);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "STOP";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(0, 180);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(179, 31);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "START";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.Transparent;
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(6, 122);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(340, 55);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Connect to Arduino Serial (COM)";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "COM1",
+            "COM2",
+            "COM3",
+            "COM4",
+            "COM5",
+            "COM6"});
+            this.comboBox1.Location = new System.Drawing.Point(246, 26);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(82, 22);
+            this.comboBox1.TabIndex = 13;
+            this.comboBox1.Text = "COM5";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 29);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(279, 14);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Choose a serial port that you wish to connect to";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Enabled = false;
+            this.pictureBox1.Image = global::Project_Aultra.Properties.Resources.PC1i;
+            this.pictureBox1.Location = new System.Drawing.Point(3, 32);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(353, 175);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 11;
+            this.pictureBox1.TabStop = false;
+            // 
+            // txtobj
+            // 
+            this.txtobj.BackColor = System.Drawing.Color.Red;
+            this.txtobj.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtobj.ForeColor = System.Drawing.Color.OliveDrab;
+            this.txtobj.Location = new System.Drawing.Point(25, 48);
+            this.txtobj.Multiline = true;
+            this.txtobj.Name = "txtobj";
+            this.txtobj.Size = new System.Drawing.Size(34, 139);
+            this.txtobj.TabIndex = 12;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial Rounded MT Bold", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Transparent;
+            this.label3.Location = new System.Drawing.Point(12, 3);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(184, 24);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Third Eye Project";
+            // 
+            // button3
+            // 
+            this.button3.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.button3.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.button3.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Red;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.ForeColor = System.Drawing.Color.White;
+            this.button3.Location = new System.Drawing.Point(318, 3);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(32, 21);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "X";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(360, 511);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtobj);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.groupBox2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ProgressBar prgsCM;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox txtobj;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label lblmessage;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ c# programming/Project_Aultra/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Speech.Synthesis;
+namespace Project_Aultra
+{
+    public partial class Form1 : Form
+    {
+        SerialPort _serialPort;
+        SpeechSynthesizer reader;
+        delegate void SetTextDelegate(string value); 
+        public Form1()
+        {
+
+            InitializeComponent();
+            button2.Enabled = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            _serialPort = new SerialPort();
+            reader = new SpeechSynthesizer();
+             _serialPort.BaudRate = 9600;
+             _serialPort.Parity = Parity.None;
+             _serialPort.DataBits = 8;
+             _serialPort.ReadTimeout = 500;
+             _serialPort.WriteTimeout = 500;
+             _serialPort.DataReceived += new SerialDataReceivedEventHandler(DataReceivedHandler);
+             txtobj.Visible = false;
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            _serialPort.PortName = comboBox1.Text;
+            try
+            {
+                if (!_serialPort.IsOpen)
+                {
+                    _serialPort.Open();
+                    pictureBox1.Enabled = true;
+                    button2.Enabled = true;
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Error opening the serial port!");
+            }
+
+        }
+        public void SetText(string value)
+        {
+            if (InvokeRequired)
+                try
+                {
+                    Invoke(new SetTextDelegate(SetText), value);
+                }
+                catch { }
+            else
+                label1.Text = value;
+        }
+
+        private void DataReceivedHandler(object sender, SerialDataReceivedEventArgs e)
+        {
+            string indata = _serialPort.ReadLine();
+            SetText(indata);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+           
+                    _serialPort.Close();
+                    pictureBox1.Enabled = false;
+                     lblmessage.Text = "Application Paused";
+                reader.Pause();
+             
+        }
+        private void label1_TextChanged(object sender, EventArgs e)
+        {
+            if (int.Parse(label1.Text) <= 100)
+            {
+
+                reader.Resume();
+                lblmessage.ForeColor = Color.Red;
+                lblmessage.Text = "Be carefull an object in front of you";
+                reader.SpeakAsync("Be carefull an object in front of you");
+                txtobj.Visible = true;
+                prgsCM.Value = (int)(int.Parse(label1.Text) /* * 0.25 */ );
+              pictureBox1.Enabled = false;
+            }
+            else
+            {
+                lblmessage.ForeColor = Color.Green;
+                lblmessage.Text = "Go a head";
+                pictureBox1.Enabled = true;
+                prgsCM.Value = 100;
+                txtobj.Visible = false;
+                reader.Pause();
+            }
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
