de8da2a6a1ec06c5f425c7d8507670da1d81150b
taiabur rahman <taiabur-tju@qq.com>
2018-11-04 20:05:44 +0800

    mobile application

---
+++ BlindManEyes_ProjectAPPinventor/src/appinventor/ai_jim2017cn/BlindManEyes_ProjectAPPinventor/Home.scm
@@ -0,0 +1,4 @@
+#|
+$JSON
+{"authURL":["ai2.appinventor.mit.edu"],"YaVersion":"172","Source":"Form","Properties":{"$Name":"Home","$Type":"Form","$Version":"23","AppName":"BlindManEyes_copy_3","Title":"Home","Uuid":"0","$Components":[{"$Name":"Clock1","$Type":"Clock","$Version":"3","TimerInterval":"5000","Uuid":"-1283090722"}]}}
+|#
